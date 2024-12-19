f400fa3546199f61f4dfc1143e97fbec7f67b20a
Pichayut Sombat <psb.nnt@gmail.com>
2024-12-20 00:39:31 +0700

    add additional logging info
    for development purpose

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
 const logger = express.Router();
+const d = new Date();
 
 logger.use((req, res, next) => {
-    console.log(req.method, req.url);
+    console.log(d.toLocaleTimeString(Date.now()), req.socket.remoteAddress, req.method, req.url);
     next();
 });
 
