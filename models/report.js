8ae1004b13e666a130adbad92f6f67d49c14d4e7
Pichayut Sombat <psb.nnt@gmail.com>
2024-12-20 00:42:27 +0700

    change table report > reports

--- models/report.js
+++ models/report.js
@@ -1,27 +1,27 @@
 import db from '../config/conn.js';
 
 const createReport = async (data) => {
-    const stmt = db.prepare('INSERT INTO report (deviceID, time, status) VALUES (?, ?, ?)');
+    const stmt = db.prepare('INSERT INTO reports (deviceID, time, status) VALUES (?, ?, ?)');
     return stmt.run(data.deviceID, data.time, data.status);
 };
 
 const listReport = async () => {
-    const stmt = db.prepare('SELECT * FROM report');
+    const stmt = db.prepare('SELECT * FROM reports');
     return stmt.all();
 };
 
 const listReportByDevice = async (data) => {
-    const stmt = db.prepare('SELECT * FROM report WHERE deviceID = ?');
+    const stmt = db.prepare('SELECT * FROM reports WHERE deviceID = ?');
     return stmt.get(data.deviceID);
 };
 
 const updateReport = async (data) => {
-    const stmt = db.prepare('UPDATE report SET time = ?, status = ? WHERE deviceID = ?');
-    return stmt.run(data.time, data.status, data.deviceID);
+    const stmt = db.prepare('UPDATE reports SET status = ? WHERE deviceID = ?');
+    return stmt.run(data.status, data.deviceID);
 };
 
 const deleteReport = async (data) => {
-    const stmt = db.prepare('DELETE FROM report WHERE deviceID = ?');
+    const stmt = db.prepare('DELETE FROM reports WHERE deviceID = ?');
     return stmt.run(data.deviceID);
 };
 
